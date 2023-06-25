d1d4bc33f6f9f2b76b227e2917f7895b4866effc
fatemanazari <fatemanazari78@gmail.com>
2023-06-25 20:56:21 +0430

    Update js file

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+//Mobile Menu
+
+  //Select classes
+
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+
+  //Add event listener to open the menu when clicked on
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
+  //Select each nav links and add event listener to close the menu when clicked on
+
 document.querySelectorAll('.nav-link').forEach((n) => n
   .addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
   }));
+
+//Form validation
+

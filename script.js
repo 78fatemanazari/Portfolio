210dde750ea7f157e3e2174a298997ab8fa56638
fatemanazari <fatemanazari78@gmail.com>
2023-06-25 21:20:03 +0430

    Add margin

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-//Mobile Menu
-
-  //Select classes
-
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
-  //Add event listener to open the menu when clicked on
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
-  //Select each nav links and add event listener to close the menu when clicked on
 
 document.querySelectorAll('.nav-link').forEach((n) => n
   .addEventListener('click', () => {
@@ -20,5 +14,4 @@ document.querySelectorAll('.nav-link').forEach((n) => n
     navMenu.classList.remove('active');
   }));
 
-//Form validation
 

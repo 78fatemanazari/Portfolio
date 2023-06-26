7c334fe1635ce76e04aa1d9ebc716d986c1ad479
fatemanazari <fatemanazari78@gmail.com>
2023-06-27 00:36:41 +0430

    Update js file

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const navMenu = document.querySelector('.nav-menu');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+  /*navMenu.classList.toggle('active');*/
 });
 
 
 document.querySelectorAll('.nav-link').forEach((n) => n
   .addEventListener('click', () => {
     hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
+    /*navMenu.classList.remove('active');*/
   }));
 
 

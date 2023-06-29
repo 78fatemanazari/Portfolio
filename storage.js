875a470189c14469a955a7473074f86353a1f3d6
Shahid Rafique Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-06-29 20:37:47 +0530

    fix eslint error

--- storage.js
+++ storage.js
@@ -1,33 +1,33 @@
 function saveDataForm() {
-    const inputs = document.querySelectorAll('input');
+  const inputs = document.querySelectorAll('input');
   
-    const dataForm = {};
+  const dataForm = {};
   
-    inputs.forEach((input) => {
-      dataForm[input.id] = input.value;
-    });
+  inputs.forEach((input) => {
+    dataForm[input.id] = input.value;
+  });
   
-    localStorage.setItem('dataForm', JSON.stringify(dataForm));
+  localStorage.setItem('dataForm', JSON.stringify(dataForm));
   }
   
-  function loadFormData() {
-    const savedData = localStorage.getItem('dataForm');
+function loadFormData() {
+  const savedData = localStorage.getItem('dataForm');
   
-    if (savedData) {
-      const dataForm = JSON.parse(savedData);
+  if (savedData) {
+    const dataForm = JSON.parse(savedData);
   
-      for (const field in dataForm) {
-        if (dataForm.hasOwnProperty(field)) {
-          document.getElementById(field).value = dataForm[field];
-        }
+    for (const field in dataForm) {
+      if (dataForm.hasOwnProperty(field)) {
+        document.getElementById(field).value = dataForm[field];
       }
     }
   }
+}
   
-  const inputs = document.querySelectorAll('input');
-  inputs.forEach((input) => {
-    input.addEventListener('change', saveDataForm);
-  });
+const inputs = document.querySelectorAll('input');
+inputs.forEach((input) => {
+  input.addEventListener('change', saveDataForm);
+});
   
-  //window.addEventListener('load', loadFormData);
+window.addEventListener('load', loadFormData);
   

c5486a892eae5b4edf2a20db627f06e0539f6ad4
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-06-29 21:10:54 +0530

    small fix in eslintrc.json

--- storage.js
+++ storage.js
@@ -1,33 +1,32 @@
 function saveDataForm() {
   const inputs = document.querySelectorAll('input');
-  
+
   const dataForm = {};
-  
+
   inputs.forEach((input) => {
     dataForm[input.id] = input.value;
   });
-  
+
   localStorage.setItem('dataForm', JSON.stringify(dataForm));
-  }
-  
+}
+
 function loadFormData() {
   const savedData = localStorage.getItem('dataForm');
-  
+
   if (savedData) {
     const dataForm = JSON.parse(savedData);
-  
-    for (const field in dataForm) {
-      if (dataForm.hasOwnProperty(field)) {
+
+    Object.keys(dataForm).forEach((field) => {
+      if (Object.prototype.hasOwnProperty.call(dataForm, field)) {
         document.getElementById(field).value = dataForm[field];
       }
-    }
+    });
   }
 }
-  
+
 const inputs = document.querySelectorAll('input');
 inputs.forEach((input) => {
   input.addEventListener('change', saveDataForm);
 });
-  
+
 window.addEventListener('load', loadFormData);
-  
